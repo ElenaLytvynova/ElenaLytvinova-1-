59b1c61c6cd63e94850fbb73d3493650236b497d
ElenaLytvynova <aragwena@gmail.com>
2020-01-31 11:21:46 +0200

    First commit

---
+++ effects.php
@@ -0,0 +1,235 @@
+    <?php
+    include "header.php";
+    ?>
+
+    <main role="main">
+
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h2 class="text-center">Effects JqueryUi</h2>
+          </div>
+        </div>        
+        <div class="row">
+          <div class="col-md-4">
+            <div class="card">
+              <img src="img/pic1.jpg" alt="" class="card-img-top pic1">
+              <div class="card-body">
+                <div class="card-title">Card1</div>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis minus maiores omnis dicta natus, minima, ab consectetur quod aliquam, ipsum nobis porro facere. Dolorum consectetur, quisquam dolore minima perferendis id.</p>
+              </div>
+              <div class="card-footer">
+                <button class="btn btn-lg btn-warning btnef1">Effects</button>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+           <div class="card">
+            <img src="img/pic2.jpg" alt="" class="card-img-top pic2">
+            <div class="card-body">
+              <div class="card-title">Card2</div>
+              <p class="card-text abz2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis minus maiores omnis dicta natus, minima, ab consectetur quod aliquam, ipsum nobis porro facere. Dolorum consectetur, quisquam dolore minima perferendis id.</p>
+            </div>
+            <div class="card-footer">
+              <button class="btn btn-lg btn-warning btnef2">Effects</button>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+         <div class="card">
+          <img src="img/pic3.jpg" alt="" class="card-img-top pic3">
+          <div class="card-body">
+            <div class="card-title">Card3</div>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis minus maiores omnis dicta natus, minima, ab consectetur quod aliquam, ipsum nobis porro facere. Dolorum consectetur, quisquam dolore minima perferendis id.</p>
+          </div>
+          <div class="card-footer">
+            <button class="btn btn-lg btn-warning btnef3">Effects</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container mt-4">
+      <div class="row">
+        <div class="col-md-12">
+          <h2 class="text-centr">Show/hide</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="card">
+            <img src="img/pic4.jpg" alt="" class="card-img-top pic4 hide">
+            <div class="card-body">
+              <div class="card-title">Card4</div>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis minus maiores omnis dicta natus, minima, ab consectetur quod aliquam, ipsum nobis porro facere. Dolorum consectetur, quisquam dolore minima perferendis id.</p>
+            </div>
+            <div class="card-footer">
+              <button class="btn btn-lg btn-warning btnshow">Show</button>
+            </div>          
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="card">
+            <img src="img/pic5.jpg" alt="" class="card-img-top pic5">
+            <div class="card-body">
+              <div class="card-title">Card5</div>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis minus maiores omnis dicta natus, minima, ab consectetur quod aliquam, ipsum nobis porro facere. Dolorum consectetur, quisquam dolore minima perferendis id.</p>
+            </div>
+            <div class="card-footer">
+              <button class="btn btn-lg btn-danger btnhide">Effects</button>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+    <div class="container mt-4">
+      <div class="row">
+        <div class="col-md-12">
+          <h2 class="text-center">AddClass/RemoveClass/ToggleClass/SwitchClass</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="card">          
+          <div class="card-body">
+            <div class="card-title">Card6</div>
+            <p class="card-text text1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis minus maiores omnis dicta natus, minima, ab consectetur quod aliquam, ipsum nobis porro facere. Dolorum consectetur, quisquam dolore minima perferendis id.</p>
+          </div>
+          <div class="card-footer">
+            <button class="btn btn-lg btn-danger btnadd">Add class</button>
+          </div>
+        </div>
+        </div>
+        <div class="col-md-6">
+          <div class="card">          
+          <div class="card-body">
+            <div class="card-title">Card7</div>
+            <p class="card-text text2 font">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis minus maiores omnis dicta natus, minima, ab consectetur quod aliquam, ipsum nobis porro facere. Dolorum consectetur, quisquam dolore minima perferendis id.</p>
+          </div>
+          <div class="card-footer">
+            <button class="btn btn-lg btn-danger btnremove">Remove class</button>
+          </div>
+        </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="card">          
+          <div class="card-body">
+            <div class="card-title">Card8</div>
+            <p class="card-text text3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis minus maiores omnis dicta natus, minima, ab consectetur quod aliquam, ipsum nobis porro facere. Dolorum consectetur, quisquam dolore minima perferendis id.</p>
+          </div>
+          <div class="card-footer">
+            <button class="btn btn-lg btn-danger btntoggle">Toggle class</button>
+          </div>
+        </div>
+        </div>
+        <div class="col-md-6">
+          <div class="card">          
+          <div class="card-body">
+            <div class="card-title">Card9</div>
+            <p class="card-text text4 font">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis minus maiores omnis dicta natus, minima, ab consectetur quod aliquam, ipsum nobis porro facere. Dolorum consectetur, quisquam dolore minima perferendis id.</p>
+          </div>
+          <div class="card-footer">
+            <button class="btn btn-lg btn-danger btnswitch">Switch class</button>
+          </div>
+        </div>
+        </div>
+      </div>
+     <div class="row">
+       <div class="col-md-12">
+         <h2 class="text-center">Animate color</h2>
+       </div>
+     </div>
+     <div class="row">
+       <div class="col-md-6">
+         <div class="card">            
+          <div class="card-title">Card10</div>
+            <div class="card-body">
+              <div class="coloranim">Color Animate</div>
+          </div>
+          <div class="card-footer">
+            <button class="btn btn-lg btn-info btnanim">Color Animate</button>
+          </div>
+        </div>
+       </div>
+     </div>
+    </div>
+
+      <!-- Marketing messaging and featurettes
+        ================================================== -->
+        <!-- Wrap the rest of the page in another container to center all the content. -->
+
+        <div class="container marketing">
+
+          <!-- Three columns of text below the carousel -->
+          <div class="row">
+            <div class="col-lg-4">
+              <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+              <h2>Heading</h2>
+              <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+              <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+            </div><!-- /.col-lg-4 -->
+            <div class="col-lg-4">
+              <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+              <h2>Heading</h2>
+              <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+              <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+            </div><!-- /.col-lg-4 -->
+            <div class="col-lg-4">
+              <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+              <h2>Heading</h2>
+              <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+              <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+            </div><!-- /.col-lg-4 -->
+          </div><!-- /.row -->
+
+
+          <!-- START THE FEATURETTES -->
+
+          <hr class="featurette-divider">
+
+          <div class="row featurette">
+            <div class="col-md-7">
+              <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
+              <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+            </div>
+            <div class="col-md-5">
+              <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+            </div>
+          </div>
+
+          <hr class="featurette-divider">
+
+          <div class="row featurette">
+            <div class="col-md-7 order-md-2">
+              <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
+              <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+            </div>
+            <div class="col-md-5 order-md-1">
+              <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+            </div>
+          </div>
+
+          <hr class="featurette-divider">
+
+          <div class="row featurette">
+            <div class="col-md-7">
+              <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
+              <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+            </div>
+            <div class="col-md-5">
+              <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+            </div>
+          </div>
+
+          <hr class="featurette-divider">
+
+          <!-- /END THE FEATURETTES -->
+
+        </div><!-- /.container -->
+
+        <?php
+        include "footer.php";
+        ?>
---
+++ header.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+      <title>Carousel Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/carousel.css" rel="stylesheet">
+    <link href="libs/jqueryui/jquery-ui.css" rel="stylesheet">
+    <link href="libs/colorbox/colorbox.css" rel="stylesheet">
+    <link href="libs/superfish/css/superfish.css" rel="stylesheet">
+    <link href="libs/superfish/css/superfish-vertical.css" rel="stylesheet">
+    <link href="libs/superfish/css/superfish-navbar.css" rel="stylesheet">
+    <link href="libs/superfish/css/megafish.css" rel="stylesheet">
+    <!-- Start WOWSlider.com HEAD section -->
+    <link rel="stylesheet" type="text/css" href="engine1/style.css" />
+    <!-- End WOWSlider.com HEAD section -->     
+    <link href="libs/bxslider/jquery.bxslider.css" rel="stylesheet">  
+    <link rel="stylesheet" href="css/farbtastic.css" type="text/css" />       
+     <!-- <link rel="stylesheet" href="libs/parallax/css/jquery.parallax.css" type="text/css" /> -->
+    <link href="css/style.css" rel="stylesheet">
+       
+  </head>
+  <body>
+
+    <header>
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="#">Carousel</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="effects.php">Effects JqueryUI</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="widgets.php">Widjets JqueryUI</a>
+            </li>
+             <li class="nav-item">
+              <a class="nav-link" href="actions.php">Interaction JqueryUI</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="galery.php">Galery</a>
+            </li>
+             <li class="nav-item">
+              <a class="nav-link" href="menu.php">Superfish Menu</a>
+            </li>
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Sliders
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+               <a href="wowslider.php" class="dropdown-item">Wowslider</a>
+               <a href="bxslider.php" class="dropdown-item">BxSlider</a>
+               <a href="forma.php" class="dropdown-item">Форма обратной связи</a>
+               <a href="Farbtastic.php" class="dropdown-item">Farbtastic</a>
+               <a href="parallax.php" class="dropdown-item">Parallax</a>
+            </div>
+          </ul>          
+        </div>
+      </nav>
+    </header>
